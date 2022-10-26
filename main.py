d3a64595dac4b18cc181090dce47960969da2fa7
bigJIU <1572204178@qq.com>
2022-10-26 11:49:14 +0800

    Week7 done

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
+import os
+
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__, static_folder='')
 
-bufferJson = ['']
+replayDataPath = "reps/"
+evalDataPath = "evals/"
 
 @app.route('/')
 def index():
     py2htmlstr = 'py2html test str'
     return render_template('play.html', py2htmlstr=py2htmlstr)
-
-
-@app.route('/datapage')
-def datapage():
-    return bufferJson
 
 
 @app.route('/eval')
@@ -23,18 +21,25 @@ def eval():
 @app.route('/', methods=['POST'])
 def getJSONData():
     if request.method == 'POST':
-        print("POST")
+        print("POST Game")
         print(request.json)
-        return redirect(url_for('datapage'))
+        saveFile(replayDataPath, request.json[4], request.json)
+    return "Catch JSON Data"
 
-
-@app.route('/', methods=['POST'])
+@app.route('/eval', methods=['POST'])
 def getRadioData():
     if request.method == 'POST':
-        print("POST")
+        print("POST Eval")
         print(request.json)
-        return redirect(url_for('datapage'))
+        saveFile(evalDataPath,"eval",request.json[0]+request.json[1]+request.json[2])
+    return "catch Radio"
 
+def saveFile(path,filename,content):
+    file_dir = os.path.join(os.getcwd(), path)
+    file_path = os.path.join(file_dir, filename+".txt")
+    f = open(file_path, "w", encoding="utf8")
+    f.write(content)
+    f.close()
 
 if __name__ == '__main__':
     app.debug = True
